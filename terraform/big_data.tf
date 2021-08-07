295a5fbc557842e51edb86117f10629b1c4225ea
Pas Apicella <pas.apicella@gmail.com>
2021-08-07 20:47:01 +1000

    Initial work on workshop

---
+++ terraform/big_data.tf
@@ -0,0 +1,37 @@
+resource google_sql_database_instance "master_instance" {
+  name             = "terragoat-${var.environment}-master"
+  database_version = "POSTGRES_11"
+  region           = var.region
+
+  settings {
+    tier = "db-f1-micro"
+    ip_configuration {
+      ipv4_enabled = true
+      authorized_networks {
+        name  = "WWW"
+        value = "0.0.0.0/0"
+      }
+    }
+    backup_configuration {
+      enabled = false
+    }
+  }
+}
+
+resource google_bigquery_dataset "dataset" {
+  dataset_id = "terragoat_${var.environment}_dataset"
+  access {
+    special_group = "allAuthenticatedUsers"
+    role          = "READER"
+  }
+  labels = {
+    git_commit           = "83661b5e88dd3768c10dbcfa2050c04d85b62fa8"
+    git_file             = "terraform/gcp/big_data.tf"
+    git_last_modified_at = "2020-07-09 13:34:36"
+    git_last_modified_by = "nimrodkor@gmail.com"
+    git_modifiers        = "nimrodkor"
+    git_org              = "bridgecrewio"
+    git_repo             = "terragoat"
+    yor_trace            = "2560d883-bc3a-4cb6-b9fc-fb666edf626e"
+  }
+}
